f8dd9b647d9757aa9912c95c69af53703ce6fb0e
P1kxel <dmit.ziborov@gmail.com>
2026-06-01 22:01:38 +0300

    Update add.sh

--- add.sh
+++ add.sh
@@ -5,25 +5,25 @@ vim import_user.sh
 csv_file="$1"
 
 # Create OU
-awk -F ';' 'NR>1 {print $5}' "csv_file" | sort | uniq | while read ou;
+awk -F ';' 'NR>1 {print $5}' "$csv_file" | sort | uniq | while read ou;
 do
-samba-tool ou add OU="$ou",DC=au-team,DC=irpo;
+        samba-tool ou add OU="$ou",DC=au-team,DC=irpo;
 done
 # Create Users
 while IFS=";" read -r firstName lastName role phone ou street zip city country password;
 do
-if [ "$firstName" == "First Name" ];
-then
-continue
-fi
+        if [ "$firstName" == "First Name" ];
+        then
+                continue
+        fi
 
-username="${firstName,,}.${lastName,,}"
+        username="${firstName,,}.${lastName,,}"
 
-samba-tool user add "$username" P@ssw0rd1 \
---given-name="$firstname" \
---surname="$lastname" \
---telephone-number="$phone" \
---job-title="$role" \
---userou="OU=$ou"
-samba-tool user setexpiry "$username" --noexpiry
+        samba-tool user add "$username" P@ssw0rd1 \
+                --given-name="$firstName" \
+                --surname="$lastName" \
+                --telephone-number="$phone" \
+                --job-title="$role" \
+                --userou="OU=$ou"
+        samba-tool user setexpiry "$username" --noexpiry
 done < "$csv_file"
